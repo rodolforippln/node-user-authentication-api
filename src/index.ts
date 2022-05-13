6500f91f999f8b97ca2f5101f4621e2277aea6e0
Rodolfo Hernandez <rodolforippln@gmail.com>
2022-05-13 00:43:25 -0300

    Adição dos end-points responsáveis pelo CRUD de Usuários e Status

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import express, { Request, Response, NextFunction } from "express"
+import express from "express"
+import statusRoute from "./routes/status.route"
+import usersRoute from "./routes/users.route"
 
 const app = express()
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'bar 1' })
-})
+// Configuração da aplicação
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
+// Configuração de Rotas
+app.use(usersRoute)
+app.use(statusRoute)
+
+// Iniciando Servidor
 app.listen(3000, () => {
     console.log('Aplicação rodando na porta 3000!')
 })
